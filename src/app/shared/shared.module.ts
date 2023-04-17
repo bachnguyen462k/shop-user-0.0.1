17df0d1a4823a983b0915fb6a27fac20efb77714
bach nguyen <nguyenbach.dev@gmail.com>
2023-04-17 22:29:19 +0700

    commit 1

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,6 +8,7 @@ import { ArticleListComponent, ArticleMetaComponent, ArticlePreviewComponent } f
 import { FavoriteButtonComponent, FollowButtonComponent } from './buttons';
 import { ListErrorsComponent } from './list-errors.component';
 import { ShowAuthedDirective } from './show-authed.directive';
+import { LoadingComponent } from './loading/loading.component';
 
 @NgModule({
   imports: [
@@ -24,7 +25,8 @@ import { ShowAuthedDirective } from './show-authed.directive';
     FavoriteButtonComponent,
     FollowButtonComponent,
     ListErrorsComponent,
-    ShowAuthedDirective
+    ShowAuthedDirective,
+    LoadingComponent
   ],
   exports: [
     ArticleListComponent,
